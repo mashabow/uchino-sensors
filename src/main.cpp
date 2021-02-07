3f4e020102a29b6170d69738befc8bbbafb87226
Masaya Nakamura <mashabow@shiromoji.net>
2021-02-07 17:31:00 +0900

    feat: get temperature and humidity from DHT11 sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <Arduino.h>
+#include "DHT.h"
+
+#define DHTPIN 4      // IO4 ピンからセンサーの値を得る
+#define DHTTYPE DHT11 // DHT 11 を使う
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Hello, world!");
+  dht.begin();
 }
 
 void loop() {
-  Serial.println("Hi!");
-  delay(1000);
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  Serial.println("Temperature: " + String(t, 1) + "°C\tHumidity: " + String(h, 0) + "%");
+
+  delay(2000);
 }
