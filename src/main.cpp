eeebd50554d9e88f260a7a202ce14132cc94e6ab
Masaya Nakamura <mashabow@shiromoji.net>
2021-04-11 21:29:45 +0900

    feat: send data with MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,21 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include "secrets.h"
 #include "DHT.h"
 
 #define DHTPIN 4      // IO4 ピンからセンサーの値を得る
 #define DHTTYPE DHT11 // DHT 11 を使う
 
-WiFiClient client;
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup()
+void setupWiFi()
 {
-  Serial.begin(115200);
-
-  // WiFi に接続する
-
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  Serial.print("Connecting");
+  Serial.print("Connecting WiFi");
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
@@ -29,17 +27,52 @@ void setup()
   Serial.println(WiFi.localIP());
 
   WiFi.printDiag(Serial);
+}
 
-  // DHT 初期化
+void setupMQTT()
+{
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+}
 
+void reconnectMQTT()
+{
+  while (!mqttClient.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+
+    if (mqttClient.connect("ESP8266Client"))
+    {
+      Serial.println("connected");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  setupWiFi();
+  setupMQTT();
   dht.begin();
 }
 
 void loop()
 {
-  float t = dht.readTemperature();
-  float h = dht.readHumidity();
-  Serial.println("Temperature: " + String(t, 1) + "°C\tHumidity: " + String(h, 0) + "%");
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  Serial.println("Temperature: " + String(temperature, 1) + "°C\t" +
+                 "Humidity: " + String(humidity, 0) + "%");
+
+  String data = String(temperature, 1) + "," + String(humidity, 0);
+
+  reconnectMQTT();
+  mqttClient.publish("sample_topic", data.c_str());
 
   delay(2000);
 }
