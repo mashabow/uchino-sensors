020e3a5b14ffe42a0ab1de38aa478eb2468a4d8a
Masaya Nakamura <mashabow@shiromoji.net>
2021-04-11 19:38:31 +0900

    feat: connect to a WiFi access point

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,37 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "secrets.h"
 #include "DHT.h"
 
 #define DHTPIN 4      // IO4 ピンからセンサーの値を得る
 #define DHTTYPE DHT11 // DHT 11 を使う
 
+WiFiClient client;
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
   Serial.begin(115200);
+
+  // WiFi に接続する
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  WiFi.printDiag(Serial);
+
+  // DHT 初期化
+
   dht.begin();
 }
 
