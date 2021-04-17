e9b6aeca4d459104488c8e6d27d8bd8b2992fb23
Masaya Nakamura <m-nakamura@feedforce.jp>
2021-04-17 19:02:58 +0900

    feat: get measurements from API with Amplify GraphQL Client
    https://docs.amplify.aws/lib/graphqlapi/query-data/q/platform/js

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import Amplify from 'aws-amplify';
+import React, { useEffect, useState } from 'react';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { GraphQLResult } from '@aws-amplify/api-graphql';
 
 import awsExports from './aws-exports';
-import logo from './logo.svg';
+import * as queries from './graphql/queries';
+import { ListMeasurementsQuery, Measurement } from './api';
 import './App.css';
 
 Amplify.configure(awsExports);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  const [measurements, setMeasurements] = useState<readonly Measurement[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = (await API.graphql(
+        graphqlOperation(queries.listMeasurements)
+      )) as GraphQLResult<ListMeasurementsQuery>;
+
+      setMeasurements(
+        (data?.listMeasurements?.items?.filter(
+          Boolean
+        ) as readonly Measurement[]) ?? []
+      );
+    })();
+  }, []);
+
+  return <div className="App">{JSON.stringify(measurements)}</div>;
+};
 
 export default App;
