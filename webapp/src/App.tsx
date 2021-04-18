3e5a10a21353a51f1ad2ea548b1c17ef0d64a90f
Masaya Nakamura <m-nakamura@feedforce.jp>
2021-04-19 02:15:56 +0900

    feat: add GitHub link to the header

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -7,6 +7,7 @@ import * as queries from './graphql/queries';
 import { ListMeasurementsQuery, Measurement } from './api';
 import './App.css';
 import Charts from './Charts';
+import GitHubMark from './github-mark.svg';
 
 Amplify.configure(awsExports);
 
@@ -29,7 +30,22 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      <h1 className="Title">uchino-sensors</h1>
+      <header>
+        <h1 className="Title">uchino-sensors</h1>
+        <a
+          className="GitHubLink"
+          href="https://github.com/mashabow/uchino-sensors"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={GitHubMark}
+            alt="GitHub リポジトリへ"
+            width="18"
+            height="18"
+          />
+        </a>
+      </header>
       <Charts measurements={measurements} />
     </div>
   );
